0a5431dfef89f88305896e622bf460290f4b704e
SanketNaykal <naykalsanket@gmail.com>
2025-11-13 16:06:44 +0530

    modifiy: home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('SighupPage');
+        return view('LoginPage');
     }
     public function login()
     {
