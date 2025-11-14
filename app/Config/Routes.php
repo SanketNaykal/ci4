c5340065f9cf913531b50d8d9e7d9efbb07be21e
SanketNaykal <naykalsanket@gmail.com>
2025-11-14 16:58:51 +0530

    dbtest routes, dbtest return change

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,10 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('dbtest', 'Home::dbtest');
 $routes->get('login',    'Auth::login');
 $routes->post('login',   'Auth::attemptLogin');
 $routes->get('signup',   'Auth::signup');
 $routes->post('signup',  'Auth::register');
 $routes->get('logout',   'Auth::logout');
-$routes->get('dbtest', 'Home::dbtest');
 //$routes->setAoutoRoute(true);
